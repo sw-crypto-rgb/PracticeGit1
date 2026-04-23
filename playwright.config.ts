7beb524e67b1355741894cf877d44504769e466b
sashi <wunnava.sashi@gmail.com>
2026-04-23 19:14:33 -0400

    Push 5

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,12 +29,12 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    screenshot: 'on',
+    screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
     testIdAttribute: 'data-test',
   },
-
+timeout: 10000,
   /* Configure projects for major browsers */
   projects: [
     {
@@ -47,10 +47,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/AnnotationPractice.spec.ts
@@ -0,0 +1,31 @@
+import { test } from "@playwright/test";
+
+test("Practice Test 1", async({page})=>{
+    console.log("Starting Practice test 1")
+    console.log("Ending Practice Test 1")
+})
+
+test("Practice Test 2", async({page})=>{
+    console.log("Starting Practice test 2")
+    console.log("Ending Practice Test 2")
+})
+
+test("Practice Test 3", async({page})=>{
+    console.log("Starting Practice test 3")
+    console.log("Ending Practice Test 3")
+})
+
+test("Practice Test 4", async({page})=>{
+    console.log("Starting Practice test 4")
+    console.log("Ending Practice Test 4")
+})
+
+test("Practice Test 5", async({page})=>{
+    console.log("Starting Practice test 5")
+    console.log("Ending Practice Test 5")
+})
+
+test("Practice Test 6", async({page})=>{
+    console.log("Starting Practice test 6")
+    console.log("Ending Practice Test 6")
+})
